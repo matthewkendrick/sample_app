9828e4b42f6c53a31d705f8ef0bc014b3ccdcbb4
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-01-17 11:34:05 +0000

    [Add]Application_chapter2

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,17 @@
 class ListsController < ApplicationController
   def new
+    @list = List.new
+  end
+
+  def create
+    #データを受け取り、新規登録するためのインスタンスを作成する
+    list = List.new(list_params)
+
+    #データをデータベースに保存するためのsaveメソッドを実行する
+    list.save
+
+    #トップ画面へリダイレクトさせる
+    redirect_to '/top'
   end
 
   def index
@@ -9,5 +21,10 @@ class ListsController < ApplicationController
   end
 
   def edit
+  end
+
+  private
+  def list_params
+    params.require(:list).permit(:title, :body)
   end
 end
