a9b01ba0dbcd4d22989c83461287776f52a4e1ab
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-01-25 02:54:08 +0000

    [Finish]Application_chapter11

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get 'lists/new'
-  get 'lists/edit'
-    get 'top' => 'home#top'
+  get 'top' => 'homes#top'
   post 'lists' => 'lists#create'
   get 'lists' => 'lists#index'
   get 'lists/:id' => 'lists#show', as: 'list'
